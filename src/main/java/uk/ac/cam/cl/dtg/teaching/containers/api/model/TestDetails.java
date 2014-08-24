2b9a81c5e32c67f267d9ebd1340a350b1c3d5746
Andrew Rice <acr31@cam.ac.uk>
2014-08-24 10:45:46 +0100

    object describing the details of a test (from test.json)

---
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/containers/api/model/TestDetails.java
@@ -0,0 +1,33 @@
+package uk.ac.cam.cl.dtg.teaching.containers.api.model;
+
+
+public class TestDetails {
+
+	private String id;
+	private String name;
+	private String[] commands;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String[] getCommands() {
+		return commands;
+	}
+
+	public void setCommands(String[] commands) {
+		this.commands = commands;
+	}
+}
